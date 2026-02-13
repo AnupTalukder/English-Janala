bde49fc1a1b0a95df2765327007dcf0eee8e99a5
AnupTalukder <anuptalukder445@gmail.com>
2026-02-13 22:53:17 +0600

    add words in lesson using api

--- script/index.js
+++ script/index.js
@@ -18,7 +18,7 @@ const displayLesson = (lessons) => {
 
         const btnDiv = document.createElement("div")
         btnDiv.innerHTML = `       
-    <button class="btn btn-outline btn-primary" > 
+    <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary" > 
     <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
     </button>
        `;
@@ -32,3 +32,60 @@ const displayLesson = (lessons) => {
 }
 
 loadLessons();
+
+
+const loadLevelWord = (id) => {
+
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayWord(data.data))
+
+}
+const displayWord = (array) => {
+    // 1. get container 
+
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+    if (array.length === 0) {
+        wordContainer.innerHTML = `
+         <div class="card-body text-center items-center col-span-full">
+         <img src="./assets/alert-error.png" width="150" height="80" >
+                <p class="bangla-font font-bold"> এই লেসন এ এখনো কোনো শব্দ যুক্ত করা হয় নি  </p>
+                <h1 id="w-m w-p" class="font-bold text-3xl"> Go To Next Lesson </h1>
+               
+            </div>
+        `;
+
+    }
+    else {
+        for (let data of array) {
+
+
+
+            const cardDiv = document.createElement("div")
+            cardDiv.innerHTML = `
+      <div class="bg-base-100 w-96 shadow-sm flex-1 text-center ">
+            <div class="card-body">
+                <h2 id="word-name"class="word font-bold"> ${data.word}
+                </h2>
+                <p>meaning/pronunciation</p>
+                <h2 id="w-m w-p" class="font-bold"> ${data.meaning} / ${data.pronunciation} </h2>
+                <div class="card-actions justify-evenly mt-3">
+                    <div class="badge badge-outline justify-start bg-blue-100 hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></div>
+                    <div class="badge badge-outline justify-end bg-blue-100 hover:bg-[#1A91FF80] "><i class="fa-solid fa-volume-high"></i></div>
+                </div>
+            </div>
+        </div>
+   
+     `;
+
+            wordContainer.append(cardDiv);
+
+        }
+
+    }
+
+}
+
